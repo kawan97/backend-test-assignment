55fd46e9ee61602d6f8dcfdf9473d2abe33b7e58
kawan97 <pshtiwankawan@gmail.com>
2022-07-24 03:53:39 -0700

    create ShowFileComponent  in blade
    create ShowFileComponent  in blade

--- backend-test-assignment/resources/views/home.blade.php
+++ backend-test-assignment/resources/views/home.blade.php
@@ -10,6 +10,8 @@
 </head>
 <body class='container mt-3'>
     Home 
+    <x-ShowFileComponent title="kawa" /> 
+
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </html>
